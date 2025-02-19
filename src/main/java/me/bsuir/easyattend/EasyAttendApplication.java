03aa2e0a3785047f973baaad79fc48cb07063a6d
gtk <gotovkkd@gmail.com>
2025-02-19 14:41:53 +0300

    bad request added

--- src/main/java/me/bsuir/easyattend/EasyAttendApplication.java
+++ src/main/java/me/bsuir/easyattend/EasyAttendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EasyAttendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasyAttendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAttendApplication.class, args);
+    }
 
 }
